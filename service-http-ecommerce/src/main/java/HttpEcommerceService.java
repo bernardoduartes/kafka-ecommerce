f04479c23c611835ad6d1ca50a985e18e0d4b60c
Bernardo Duarte <bernardoduartes@gmail.com>
2023-07-06 23:45:22 -0300

    hardcode url

--- service-http-ecommerce/src/main/java/HttpEcommerceService.java
+++ service-http-ecommerce/src/main/java/HttpEcommerceService.java
@@ -3,6 +3,8 @@ import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
 
 public class HttpEcommerceService {
+
+    // http://localhost:8080/new?email=bernardo@gmail.com&amount=70000
     public static void main(String[] args) throws Exception {
         var server = new Server(8080);
 
